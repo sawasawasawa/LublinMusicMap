c5fe325eef115f4aa94f9bfa3a30dc83a11aea85
Mateusz Sawka <m.e.sawka@gmail.com>
2017-08-12 20:01:20 +0200

    Add Houston admin panel

--- client/main.js
+++ client/main.js
@@ -1,14 +1,7 @@
-import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import App from '../imports/ui/App.js';
 import { renderRoutes } from '../imports/startup/client/routes.js';
-
 
 Meteor.startup(() => {
   injectTapEventPlugin();
-  render(<App />, document.getElementById('main-container'));
-  // render(renderRoutes(), document.getElementById('main-container'));
-  // TODO why map is not working with render routes?
 });
--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
-import createBrowserHistory from 'history/createBrowserHistory';
-// route components
+import { render } from 'react-dom';
 import App from '../../ui/App.js';
-const browserHistory = createBrowserHistory();
-export const renderRoutes = () => (
-  <Router history={browserHistory}>
-    <div>
-      <Route path="/" component={App}/>
-      {/*<Route exact path="" component={App}/>*/}
-      <Route path="lists/:id" component={App}/>
-      {/*<Route path="signin" component={AuthPageSignIn}/>*/}
-      {/*<Route path="join" component={AuthPageJoin}/>*/}
-      {/*<Route path="*" component={NotFoundPage}/>*/}
-    </div>
-    </Router>
-);
+
+Router.route('/', () => {
+  render(<App />, document.getElementById('main-container'));
+});
+
+Router.route('/elo', {
+  template: 'elo'
+});
